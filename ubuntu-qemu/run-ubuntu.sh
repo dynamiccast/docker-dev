91be9da242d29384be550c7cc9e418a2d901bfde
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-11 11:58:14 +0200

    Open port 22 to the outside on ubuntu-qemu

--- ubuntu-qemu/run-ubuntu.sh
+++ ubuntu-qemu/run-ubuntu.sh
@@ -9,5 +9,6 @@ qemu-system-x86_64 -nodefaults \
 		   -drive if=virtio,file=/ubuntu.qcow2 \
 		   -vnc :2 \
 		   -net nic,vlan=0,model=virtio \
+		   -net user,vlan=0,hostfwd=tcp::22-:22 \
 		   -vga qxl \
 		   -global qxl-vga.vram_size=33554432
