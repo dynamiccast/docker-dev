1b985210d639d94c1cfd88af8d1b8629165532a2
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-11 21:30:46 +0200

    Return apps and their path when hitting GET /apps

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,37 @@ var HOST = "ubuntu-qemu";
 var USERNAME = "vagrant";
 var PASSWORD = "vagrant";
 
+app.get('/apps', function (req, res) {
+
+  exec('for app in /usr/share/applications/*.desktop ; do app="${app##/*/}"; echo -n "${app::-8}=" && cat /usr/share/applications/${app::-8}.desktop | grep \'^Exec\' | head -n1 | cut -d \'=\' -f 2 ; done', {
+    user: USERNAME,
+    host: HOST,
+    password: PASSWORD
+  }, function(err, stdout, stderr) {
+
+    if (err) {
+      return res.status(500).send({
+	error: err,
+	stderr: stderr
+      });
+    }
+
+    var apps = stdout.split("\n");
+    var result = {};
+
+    apps.forEach(function(app) {
+
+      var appName = app.split('=')[0];
+      var appPath = app.split('=')[1];
+
+      result[appName] = appPath;
+    });
+
+    return res.send(result);
+  });
+
+});
+
 app.get('/', function (req, res) {
 
   exec('uname -a', {
