5f98db1324428f11faac4901b7cd090d99ec4f8e
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-12 13:54:54 +0200

    Add app-list component to get application list from backend

---
+++ frontend/app/components/app-list.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  appList: Ember.ArrayProxy.create([]),
+
+  getAppList: function() {
+
+    Ember.$.ajax({
+      type: "GET",
+      url: "http://localhost:3000/apps",
+    }).then((response) => {
+
+      this.get('appList').set('content', []);
+      for (var appName in response) {
+
+	this.get('appList').pushObject({
+	  appName: appName,
+	  appPath: response[appName]
+	});
+      };
+    });
+  }.on('init')
+});
